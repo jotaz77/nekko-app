fa99a819765a18c03544db0d4a221d3680d6cb68
Almir Cavalcante da Silva Junior <almir2cavalcante@gmail.com>
2026-09-18 11:46:30 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
-import { MainLayout } from "./layouts/MainLayout";
-import { Home } from "./pages/Home";
+import { MainLayout } from "./layouts/mainlayout";
+import { Home } from "./pages/home";
 
 export default function App() {
   return (
