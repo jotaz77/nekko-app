bad169259353d344fe87dae684cb8118165b0d4b
Almir Cavalcante da Silva Junior <almir2cavalcante@gmail.com>
2026-09-19 09:20:29 -0300

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import { Benefits } from "../components/landing/benefits";
 import { Hero } from "../components/landing/hero";
 import { Products } from "../components/landing/products";
+import { Plans } from "../components/landing/plans";
 
 export function Home() {
   return (
