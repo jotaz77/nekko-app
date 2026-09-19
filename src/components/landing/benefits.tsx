68c7d9cb0436837e5fe9fe4a11105be918e56014
Almir Cavalcante da Silva Junior <almir2cavalcante@gmail.com>
2026-09-19 09:09:20 -0300

    Update benefits.tsx

--- src/components/landing/benefits.tsx
+++ src/components/landing/benefits.tsx
@@ -77,7 +77,7 @@ export function Benefits() {
             </h2>
 
             <p className="mt-6 text-base leading-8 text-zinc-400 md:text-lg">
-              O NEKKO foi criada para ajudar empresas e profissionais a
+              A NEKKO foi criada para ajudar empresas e profissionais a
               trabalharem de forma mais inteligente, organizada e profissional.
             </p>
 
